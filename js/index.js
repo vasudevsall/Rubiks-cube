8d97e973b0f5d360cef62d401c3392817975f8c0
Vasu Dev Sall <vasudevsall@gmail.com>
2020-07-13 16:55:21 +0530

    Added node_modules

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { CUBE_STATE } from './cubeState.js';
 
 var canvasDiv, canvasDivStyle;
